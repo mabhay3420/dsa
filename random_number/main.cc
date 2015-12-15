72f08c713f8ffc17f6e7306689f61529b6aeeff6
Emanuel Saringan <esaringan@google.com>
2015-12-15 12:41:18 +0800

    Used const ref for the histogram param.

--- random_number/main.cc
+++ random_number/main.cc
@@ -12,7 +12,7 @@ int Generate() {
   return uniform_dist(generator);
 }
 
-void Print(std::map<int, int> hist) {
+void Print(const std::map<int, int>& hist) {
   for (const std::pair<int, int>& it : hist) {
     std::cout << std::setw(2) << it.first << ": " << it.second << std::endl;
   }
